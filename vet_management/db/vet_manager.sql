46c7b87675ee7d6fc9159b8bdbd86eb995eaf546
Alastair Smith <j.alastair.smith@outlook.com>
2021-06-05 16:39:24 +0100

    sets up tables in vet_manager.sql

---
+++ vet_management/db/vet_manager.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS pets;
+DROP TABLE IF EXISTS vets;
+
+CREATE TABLE pets(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    date_of_birth VARCHAR(255),
+    breed VARCHAR(255),
+    owner_contact_number VARCHAR(255),
+    treatment_notes TEXT,
+    vet_id INT REFERENCES vets(id)
+);
+
+CREATE TABLE vets(
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    qualifications VARCHAR(255),
+    contact_number VARCHAR(255)
+);
+
