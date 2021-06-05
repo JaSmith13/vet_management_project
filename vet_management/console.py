2e9ba5bc9087ed4b719f374a6ee40d665e96dafe
Alastair Smith <j.alastair.smith@outlook.com>
2021-06-05 21:45:03 +0100

    adds the rest of the crud methods to pet repository

--- vet_management/console.py
+++ vet_management/console.py
@@ -6,17 +6,23 @@ from models.pet import Pet
 from repositories import vet_repository, pet_repository
 
 
-vets = vet_repository.select_all()
+pet_repository.delete_all()
+#vet_repository.delete_all()
+#vets = vet_repository.select_all()
 
-
-for vet in vets:
-    print(vet.__dict__)
-
-print(vet_repository.select(2).first_name)
+pet1 = Pet('Sadie', '10/6/2020', 'scottie cross', '07833 474334', 'grumbler extraordinaire', vet_repository.select(1))
+pet_repository.save(pet1)
+# for vet in vets:
+#     print(vet.__dict__)
+pet1.name = 'Douglas'
+pet_repository.update(pet1)
+# print(vet_repository.select(2).first_name)
 
 pets = pet_repository.select_all()
 for pet in pets:
     print(pet.__dict__)
     print(pet.vet.first_name)
+
+#print(pet_repository.select(1).name)
 
 pdb.set_trace()
--- vet_management/repositories/pet_repository.py
+++ vet_management/repositories/pet_repository.py
@@ -16,11 +16,38 @@ def select_all():
     return pets
 
 #Read one 
+def select(id):
+    pet = None
+    sql = "SELECT * FROM pets WHERE id = %s"
+    values =[id]
+    result = run_sql(sql, values)[0]
+    
+    if result is not None:
+        vet = vet_repository.select(result['vet_id'])
+        pet = Pet(result['name'], result['date_of_birth'], result['breed'], result['owner_contact_number'], result['treatment_notes'], vet, result['id'])
+    return pet
 
 #Create 
+def save(pet):
+    sql = "INSERT INTO pets (name, date_of_birth, breed, owner_contact_number, treatment_notes, vet_id) VALUES (%s, %s, %s, %s, %s, %s) RETURNING id"
+    values = [pet.name, pet.date_of_birth, pet.breed, pet.owner_contact_number, pet.treatment_notes, pet.vet.id]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    pet.id =id
 
 #Update
-
-#Delete one 
+def update(pet):
+    sql = "UPDATE pets SET (name, date_of_birth, breed, owner_contact_number, treatment_notes, vet_id) = (%s, %s, %s, %s, %s, %s) WHERE id = %s"
+    values = [pet.name, pet.date_of_birth, pet.breed, pet.owner_contact_number, pet.treatment_notes, pet.vet.id, pet.id]
+    run_sql(sql, values)
 
 #Delete all
+def delete_all():
+    sql = "DELETE FROM pets"
+    run_sql(sql)
+
+#Delete one
+def delete(id):
+    sql = "DELETE FROM pets WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
