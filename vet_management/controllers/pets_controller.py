9963eeec45c11bfd64ec4923e4318fa448aedbac
Alastair Smith <j.alastair.smith@outlook.com>
2021-06-06 15:07:13 +0100

    adds view for a singel pet

--- vet_management/controllers/pets_controller.py
+++ vet_management/controllers/pets_controller.py
@@ -5,21 +5,40 @@ from models.pet import Pet
 pets_blueprint = Blueprint("pets", __name__)
 
 #INDEX
-@pets_blueprint.route("/pets")
+@pets_blueprint.route("/pets", methods=['GET'])
 def all_pets():
     pets = pet_repository.select_all()
-    return render_template("pets/index.html", pets=pets)
+    return render_template("pets/index.html", pets = pets)
+
+@pets_blueprint.route("/pets/<id>", methods=['GET'])
+def single_pet_records(id):
+    pet = pet_repository.select(id)
+    return render_template("pets/show.html", pet = pet)
 
 #NEW
-@pets_blueprint("/pets/new")
-#CREATE
+# @pets_blueprint("/pets/new", methods=['GET'])
+# def new_pet():
+#     vets = vet_repository.select_all()
+#     return render_template("pets/new.html", vets = vets)
 
-#EDIT
+# #CREATE
+# @pets_blueprint.route("/pets", methods=['POST'])
+# def create_pet():
+#     pass
 
-#UPDATE
+# #EDIT
+# @pets_blueprint.route("/pets/<id>/edit", methods=['GET'])
+# def edit_pet(id):
+#     pass
+
+# #UPDATE
+# @pets_blueprint.route("/pets/<id>", methods=['POST'])
+# def update_pet_details(id):
+#     pass
+
 
 #DELETE
-@pets_blueprint.route("/pets/<id>/delete", methods=["POST"])
+@pets_blueprint.route("/pets/<id>/delete", methods=['POST'])
 def delete_pet(id):
     pet_repository.delete(id)
     return redirect("/pets")
