47dfdf35d23400a0d7fd217643c1b7b518713ce5
Alastair Smith <j.alastair.smith@outlook.com>
2021-06-06 14:31:31 +0100

    adds delete route to pet controller and adds link on pet view

--- vet_management/controllers/pets_controller.py
+++ vet_management/controllers/pets_controller.py
@@ -4,8 +4,22 @@ from models.pet import Pet
 
 pets_blueprint = Blueprint("pets", __name__)
 
-# INDEX
+#INDEX
 @pets_blueprint.route("/pets")
 def all_pets():
     pets = pet_repository.select_all()
     return render_template("pets/index.html", pets=pets)
+
+#NEW
+@pets_blueprint("/pets/new")
+#CREATE
+
+#EDIT
+
+#UPDATE
+
+#DELETE
+@pets_blueprint.route("/pets/<id>/delete", methods=["POST"])
+def delete_pet(id):
+    pet_repository.delete(id)
+    return redirect("/pets")
